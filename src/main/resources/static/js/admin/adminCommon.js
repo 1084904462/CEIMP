39e806479122623227933959d2b1acec51660f7f
Sq <574146616@qq.com>
2017-08-31 00:19:15 +0800

    不知道为什么管理员只有概览和设计的页面可以进，其他都是404

--- src/main/resources/static/js/admin/adminCommon.js
+++ src/main/resources/static/js/admin/adminCommon.js
@@ -4,4 +4,31 @@
 layui.use(["element", "form"], function()
 {
     var element = layui.element;
+
+    //隐藏/显示侧边栏
+    $('.admin-side-toggle').on('click', function() {
+        var sideWidth = $('.layui-side').width();
+
+        if(sideWidth === 200) {
+            $('.layui-body').animate({
+                left: '0'
+            });
+            $('.layui-footer').animate({
+                left: '0'
+            });
+            $('.layui-side').animate({
+                width: '0'
+            });
+        } else {
+            $('.layui-body').animate({
+                left: '200px'
+            });
+            $('.layui-footer').animate({
+                left: '200px'
+            });
+            $('.layui-side').animate({
+                width: '200px'
+            });
+        }
+    });
 });
