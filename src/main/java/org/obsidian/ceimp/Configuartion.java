793b3431e4b24f9b22bad61f6e253bd771d5dce7
wei <1084904462@qq.com>
2017-10-25 17:58:32 +0800

    V4.7
    更换阿里爸爸德鲁伊连接池

--- src/main/java/org/obsidian/ceimp/Configuartion.java
+++ src/main/java/org/obsidian/ceimp/Configuartion.java
@@ -1,8 +1,8 @@
 package org.obsidian.ceimp;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,8 +22,8 @@ public class Configuartion {
 
     @Bean
     @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource dataSource() {
-        return new DataSource();
+    public DruidDataSource dataSource() {
+        return new DruidDataSource();
     }
 
     @Bean
