32d80af58b07a77bc4c286a68e0343c6c4f0b0ee
wei <1084904462@qq.com>
2017-11-15 13:09:25 +0800

    V1.0.14
    新增登录及奖学金接口

--- src/main/java/org/obsidian/ceimp/bean/AwardBean.java
+++ src/main/java/org/obsidian/ceimp/bean/AwardBean.java
@@ -4,6 +4,38 @@ package org.obsidian.ceimp.bean;
  * Created by BillChen on 2017/11/14.
  */
 public class AwardBean {
-    private Long awardId;
     private String awardName;
+    private String subName;
+
+    public AwardBean() {
+    }
+
+    public AwardBean(String awardName, String subName) {
+        this.awardName = awardName;
+        this.subName = subName;
+    }
+
+    @Override
+    public String toString() {
+        return "AwardBean{" +
+                "awardName='" + awardName + '\'' +
+                ", subName='" + subName + '\'' +
+                '}';
+    }
+
+    public String getAwardName() {
+        return awardName;
+    }
+
+    public void setAwardName(String awardName) {
+        this.awardName = awardName;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
 }
