fa16e641a3eaa88fa5ef346d23cc90b3dfadeff4
Archer <1091053002@qq.com>
2018-05-25 23:49:57 +0800

    新增管理员

--- src/main/java/org/obsidian/ceimp/controller/manager/ManagerSettingController.java
+++ src/main/java/org/obsidian/ceimp/controller/manager/ManagerSettingController.java
@@ -110,7 +110,7 @@ public class ManagerSettingController {
      */
     @PostMapping("/insertManager")
     @ResponseBody
-    public String insertManager(InsertManagerBean insertManagerBean){
+    public String insertManager(@RequestBody InsertManagerBean insertManagerBean){
         return JSON.toJSONString(managerService.insert(insertManagerBean));
     }
 }
